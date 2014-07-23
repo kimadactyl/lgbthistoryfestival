172cf514da005f53eba2299bb356d8f832b68390
Kim Foale <kim@alliscalm.net>
2014-07-23 16:17:17 +0100

    Initial commit

---
+++ source/javascripts/all.js
@@ -0,0 +1,7 @@
+//= require jquery/dist/jquery
+//= require foundation/js/foundation.min
+//= require foundation/js/foundation/foundation.magellan
+//= require foundation/js/foundation/foundation.equalizer
+
+//= require app
+//= require script
