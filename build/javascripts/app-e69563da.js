b539ed0ba436bd1b0352d7b8fd87bda14228b3be
Kim Foale <kim@alliscalm.net>
2014-07-24 17:21:38 +0100

    Created content for all pages, new front page

---
+++ build/javascripts/app-e69563da.js
@@ -0,0 +1,8 @@
+$(document).foundation({
+  orbit: {
+    animation: 'slide',
+    timer_speed: 5000,
+    pause_on_hover: true,
+    animation_speed: 500,
+  }
+});
--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,1 +1,8 @@
-$(document).foundation();
+$(document).foundation({
+  orbit: {
+    animation: 'slide',
+    timer_speed: 5000,
+    pause_on_hover: true,
+    animation_speed: 500,
+  }
+});
