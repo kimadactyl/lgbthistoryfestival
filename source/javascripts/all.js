8158152632147227bd555b6e0a617862fa74d901
Kim Foale <kim@alliscalm.net>
2014-12-04 14:37:06 +0000

    Probably going to scrap this but just wanted a commit

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,6 +4,7 @@
 //= require foundation/js/foundation/foundation.equalizer
 //= require jquery-validation/dist/jquery.validate.min.js
 //= require social-likes/social-likes.min
+//= require skrollr/dist/skrollr.min
 
 //= require app
 //= require script
