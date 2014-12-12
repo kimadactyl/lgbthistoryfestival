6fea9ec234aa8a232a5815d78e1da993810509ab
Kim Foale <kim@alliscalm.net>
2014-12-12 17:58:25 +0000

    Incredibly basic and glitched subnav added for front page

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,9 +1,12 @@
 $(window).scroll(function() {
 // If the current scroll position is more than the branding height minus the nav height,
 // add the sticky class to branding so we can mess with it, and hide the zigzags
-if ($(window).scrollTop() > $('#branding').height() - $('nav').height()){  
+if ($(window).scrollTop() > $('#branding').height() - $('nav#primary').height()){  
     $('body').addClass("sticky");
     $('#topborder').hide();
+    // I can't 100% justify this number but I feel that [branding height] times two works.
+    // Basically, you've scrolled [branding height] and then the actual [branding height] makes two.
+    // If that makes sense.
     $('section#main #slide-1 .small-12').css('padding-top', $('#branding').outerHeight() * 2)
   }
   else{
