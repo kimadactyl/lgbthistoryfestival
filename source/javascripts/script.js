165f4479bca60c6d23641cb355860bad3086447a
Kim Foale <kim@alliscalm.net>
2014-12-19 15:05:04 +0000

    Mostly completed #27 Had a go at #26 More work on #10

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,9 +1,11 @@
 $(window).scroll(function() {
 // If the current scroll position is more than the branding height minus the nav height,
 // add the sticky class to branding so we can mess with it, and hide the zigzags
-if ($(window).scrollTop() > $('#branding').height() - $('nav#primary').height()){  
+if ($(window).scrollTop() > $('#branding').height() + 200 - $('nav#primary').height()){  
     $('body').addClass("sticky");
     $('#topborder').hide();
+    $('#branding').hide();
+    $('#branding').slideDown();
     // I can't 100% justify this number but I feel that [branding height] times two works.
     // Basically, you've scrolled [branding height] and then the actual [branding height] makes two.
     // If that makes sense.
@@ -12,6 +14,7 @@ if ($(window).scrollTop() > $('#branding').height() - $('nav#primary').height())
   else{
     $('body').removeClass("sticky");
     $('#topborder').show();
+    $('#branding').show();
     $('section#main #slide-1 .small-12').css('padding-top', 0)
   }
 });
