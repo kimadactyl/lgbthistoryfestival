3fd1a795c86c27ec48b44eb322a7a7de7da97988
Kim Foale <kim@alliscalm.net>
2014-12-19 17:14:59 +0000

    Loads more content #10

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -4,8 +4,8 @@ $(window).scroll(function() {
 if ($(window).scrollTop() > $('#branding').height() + 200 - $('nav#primary').height()){  
     $('body').addClass("sticky");
     $('#topborder').hide();
-    $('#branding').hide();
-    $('#branding').slideDown();
+    // $('#branding').hide();
+    // $('#branding').slideDown();
     // I can't 100% justify this number but I feel that [branding height] times two works.
     // Basically, you've scrolled [branding height] and then the actual [branding height] makes two.
     // If that makes sense.
@@ -14,7 +14,7 @@ if ($(window).scrollTop() > $('#branding').height() + 200 - $('nav#primary').hei
   else{
     $('body').removeClass("sticky");
     $('#topborder').show();
-    $('#branding').show();
+    // $('#branding').show();
     $('section#main #slide-1 .small-12').css('padding-top', 0)
   }
 });
