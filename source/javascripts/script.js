6c4dd9449f01e8a46bf9418116c04cb9f3462aa3
Kim Foale <kim@alliscalm.net>
2014-12-21 14:36:58 +0000

    Hacked scalloped borders for now resolving #23

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -6,10 +6,6 @@ if ($(window).scrollTop() > $('#branding').height() + 200 - $('nav#primary').hei
     $('#topborder').hide();
     // $('#branding').hide();
     // $('#branding').slideDown();
-    // I can't 100% justify this number but I feel that [branding height] times two works.
-    // Basically, you've scrolled [branding height] and then the actual [branding height] makes two.
-    // If that makes sense.
-    $('section#main #slide-1 .small-12').css('padding-top', $('#branding').outerHeight() * 2)
   }
   else{
     $('body').removeClass("sticky");
