d43bc662fc0bf6d43033a28102a0a531e6fc3e71
Kim Foale <kim@alliscalm.net>
2014-12-23 18:01:29 +0000

    Started adding reduced sized text blocks

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,7 +2,8 @@
 //= require fastclick/lib/fastclick
 //= require foundation/js/foundation.min
 //= require social-likes/social-likes.min
-//= require jquery.stellar/jquery.stellar
+//= require jquery.stellar/jquery.stellar.min
+//= require readmore/readmore.min
 
 //= require app
 //= require script
