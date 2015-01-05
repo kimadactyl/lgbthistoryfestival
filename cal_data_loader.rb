d3e9d5090d3c0ad9b5d2d5a335ac50dccd5aae23
Phoebe Queen <foibey@gmail.com>
2015-01-05 22:29:00 +0000

    added contrib file cleaner

--- cal_data_loader.rb
+++ cal_data_loader.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+# usage: cal_data_loader.rb [clean]
+#  - clean clears contrib data file
 
 require 'icalendar'
 require 'yaml'
@@ -12,26 +14,30 @@ ICALS = {"main-festival" => "ical/main.ics",
 
 CONTRIB_FILE = "data/contribs.yml"
 
-contribs = YAML::load_file(CONTRIB_FILE)
+if ARGV[0] == "clean"
+  File.open(CONTRIB_FILE, 'w') {|f| f.write ""}
+else
+  contribs = YAML::load_file(CONTRIB_FILE)
 
-contribs ||= {} #initialize with blank hash if YAML loads false
+  contribs ||= {} #initialize with blank hash if YAML loads false
 
-ICALS.each do |file, ics|
-  ical = Icalendar.parse(open(ics)).first
-  ical.events.each do |event|
-    contributors = event.description.scan(CONTRIB_REGEX)
-    contributors.each do |contrib|
-      contrib = contrib[1..-2]
-      if !contribs.has_key?(contrib)
-        #don't overwrite existing records
-        #init new entry
-        contribs[contrib] = {"bio" => nil,
-                             "pic" => nil,
-                             "urlname" => "TODO:script urlification",
-                             "events" => event.uid.to_s}
+  ICALS.each do |file, ics|
+    ical = Icalendar.parse(open(ics)).first
+    ical.events.each do |event|
+      contributors = event.description.scan(CONTRIB_REGEX)
+      contributors.each do |contrib|
+        contrib = contrib[1..-2]
+        if !contribs.has_key?(contrib)
+          #don't overwrite existing records
+          #init new entry
+          contribs[contrib] = {"bio" => nil,
+                               "pic" => nil,
+                               "urlname" => "TODO:script urlification",
+                               "events" => event.uid.to_s}
+        end
       end
     end
   end
-end
 
-File.open(CONTRIB_FILE, 'w') {|f| f.write contribs.to_yaml } # save to file
+  File.open(CONTRIB_FILE, 'w') {|f| f.write contribs.to_yaml } # save to file
+end
