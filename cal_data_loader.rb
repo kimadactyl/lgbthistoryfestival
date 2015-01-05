b63143ea17d81741667e0b4d4f116959ea815833
Phoebe Queen <foibey@gmail.com>
2015-01-05 22:16:51 +0000

    script to fill local data with contributors from google events

---
+++ cal_data_loader.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+require 'icalendar'
+require 'yaml'
+
+CONTRIB_REGEX = /\{[^\{]*\}/
+
+ICALS = {"main-festival" => "ical/main.ics",
+  "family-space" => "ical/family-space.ics",
+  "theatre" => "ical/theatre.ics",
+  "conference" => "ical/academic.ics"}
+
+CONTRIB_FILE = "data/contribs.yml"
+
+contribs = YAML::load_file(CONTRIB_FILE)
+
+contribs ||= {} #initialize with blank hash if YAML loads false
+
+ICALS.each do |file, ics|
+  ical = Icalendar.parse(open(ics)).first
+  ical.events.each do |event|
+    contributors = event.description.scan(CONTRIB_REGEX)
+    contributors.each do |contrib|
+      contrib = contrib[1..-2]
+      if !contribs.has_key?(contrib)
+        #don't overwrite existing records
+        #init new entry
+        contribs[contrib] = {"bio" => nil,
+                             "pic" => nil,
+                             "urlname" => "TODO:script urlification",
+                             "events" => event.uid.to_s}
+      end
+    end
+  end
+end
+
+File.open(CONTRIB_FILE, 'w') {|f| f.write contribs.to_yaml } # save to file
--- get-cals.sh
+++ get-cals.sh
@@ -4,3 +4,5 @@ curl https://www.google.com/calendar/ical/lgbthistoryfestival.org_0gc2nrd6ls6fjb
 curl https://www.google.com/calendar/ical/lgbthistoryfestival.org_qtra32a7f24jpc75ghjuffeops%40group.calendar.google.com/public/basic.ics > ./ical/films.ics
 curl https://www.google.com/calendar/ical/lgbthistoryfestival.org_ijgln6bmp4th9sg6stditdv7ok%40group.calendar.google.com/public/basic.ics > ./ical/theatre.ics
 curl https://www.google.com/calendar/ical/lgbthistoryfestival.org_8mtrc289k99o4g90rc9cgce3io%40group.calendar.google.com/public/basic.ics > ./ical/academic.ics
+
+./cal_data_loader.rb
