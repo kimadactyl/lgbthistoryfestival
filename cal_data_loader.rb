80df53a2d8edb70e1525ab215ff575e687653c0d
Phoebe Queen <foibey@gmail.com>
2015-01-06 00:42:01 +0000

    doesn't make pretty *enough* unique legible event identifiers. but close

--- cal_data_loader.rb
+++ cal_data_loader.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 # usage: cal_data_loader.rb [clean]
 #  - clean clears contributor data file
+#  - warning, highly destructive, will destroy any manual additions
+#  - only exists to make development easier for the time being
 
 require 'icalendar'
 require 'yaml'
@@ -13,22 +15,50 @@ ICALS = {"main-festival" => "ical/main.ics",
   "conference" => "ical/academic.ics"}
 
 CONTRIB_FILE = "data/contribs.yml"
+EVENTS_FILE = "data/events.yml"
 
-def parameterize string
+def parameterize string, inc=false
   #avoiding requiring rails for such a simple script, for now
-  string.downcase.split(" ").join("-")
+  string.gsub(/[^\w\s\d]/,"-")
+  string = string.downcase.split(" ").join("-")
+  string = string[0..20] #truncate for usefulness
+  if inc
+    string + inc
+  else
+    string
+  end
 end
 
 if ARGV[0] == "clean"
   File.open(CONTRIB_FILE, 'w') {|f| f.write ""}
 else
   contribs = YAML::load_file(CONTRIB_FILE)
+  events = YAML::load_file(EVENTS_FILE)
 
   contribs ||= {} #initialize with blank hash if YAML loads false
+  events ||= {}
 
   ICALS.each do |file, ics|
     ical = Icalendar.parse(open(ics)).first
     ical.events.each do |event|
+
+      #might well become easier with an rdb
+      if(!events.has_key?(event.uid.to_s))
+        #new event
+        #check for clashes with pretty url
+        counter = 0
+
+        clashes = events.select {|_,url| url == parameterize(event.summary)}
+        while clashes.length > 0
+          counter += 1
+          clashes = events.select do |_,url|
+            url == parameterize(event.summary, counter.to_s)
+          end
+        end
+        #chuck counter on to avoid url collisions if necessary
+        events[event.uid.to_s] = counter > 0 ? parameterize(event.summary,counter.to_s) : parameterize(event.summary)
+      end
+
       contributors = event.description.scan(CONTRIB_REGEX)
       contributors.each do |name|
         name = name[1..-2] # cut off curly braces
@@ -48,4 +78,5 @@ else
   end
 
   File.open(CONTRIB_FILE, 'w') {|f| f.write contribs.to_yaml } # save to file
+  File.open(EVENTS_FILE, 'w') {|f| f.write events.to_yaml }
 end
