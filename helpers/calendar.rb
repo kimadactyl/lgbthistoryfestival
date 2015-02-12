8c3d10b427b6aed22df05d92598e57a8787881e9
Kim Foale <kim@alliscalm.net>
2015-02-12 15:08:26 +0000

    Ooops forgot to update recently

--- helpers/calendar.rb
+++ helpers/calendar.rb
@@ -2,13 +2,11 @@ require 'icalendar'
 module FestivalCalendar
   # Constant, accessible from any page/layout as FestivalCalendar::DAYS
   # "Thursday" => Time.new(2015,2,12),
-  DAYS = {  "Monday" => Time.new(2015,2,9),
-            "Tuesday" => Time.new(2015,2,10),
-            "Wednesday" => Time.new(2015,2,11),
+  DAYS = {  
             "Thursday" => Time.new(2015,2,12),
             "Friday" => Time.new(2015,2,13),
             "Saturday" => Time.new(2015,2,14),
-            "Sunday" => Time.new(2015,2,15) }
+            "Sunday" => Time.new(2015,2,15), }
   CONFDAYS = {
             "Friday" => Time.new(2015,2,13),
             "Saturday" => Time.new(2015,2,14),
